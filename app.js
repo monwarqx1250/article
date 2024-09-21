86f888f71d468c293cc114ad0c7016e8aed30897
monwarqx1250 <monwarh.0.1@gmail.com>
2024-09-22 00:46:09 +0600

    modified code

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
 
-const signin_email = document.getElementById("signin_email").value;
-const signin_password = document.getElementById("signin_password").value;
+// user information
+const mail = document.getElementById("signin_mail");
+const password = document.getElementById("signin_password");
 
+// file upload button
+const uploadBtn = document.getElementById("upload");
 signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener("click", () => {
-    console.log(signin_email)   // to determine the value of signin_email we are using console.log here 
   container.classList.remove("right-panel-active");
 });
+
+uploadBtn.addEventListener("change",(e) => {
+  // debug the code
+
+})
